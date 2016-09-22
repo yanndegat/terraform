812f2e7723e2415afa9cb9b567e61ab5d4be1ab4
clint shryock <clint@ctshryock.com>
2016-09-22 14:59:13 -0500

    re-go-fmt after rebase

--- builtin/providers/aws/resource_aws_emr_test.go
+++ builtin/providers/aws/resource_aws_emr_test.go
@@ -1,93 +1,93 @@
 package aws
 
 import (
-        "fmt"
-        "github.com/aws/aws-sdk-go/aws"
-        "github.com/aws/aws-sdk-go/aws/awserr"
-        "github.com/aws/aws-sdk-go/service/emr"
-        "github.com/hashicorp/terraform/helper/acctest"
-        "github.com/hashicorp/terraform/helper/resource"
-        "github.com/hashicorp/terraform/terraform"
-        "log"
-        "testing"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/emr"
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"log"
+	"testing"
 )
 
 func TestAccAWSEmrCluster_basic(t *testing.T) {
-        var jobFlow emr.RunJobFlowOutput
-        resource.Test(t, resource.TestCase{
-                PreCheck:     func() { testAccPreCheck(t) },
-                Providers:    testAccProviders,
-                CheckDestroy: testAccCheckAWSEmrDestroy,
-                Steps: []resource.TestStep{
-                        resource.TestStep{
-                                Config: testAccAWSEmrClusterConfig,
-                                Check:  testAccCheckAWSEmrClusterExists("aws_emr.tf-test-cluster", &jobFlow),
-                        },
-                },
-        })
+	var jobFlow emr.RunJobFlowOutput
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAWSEmrDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccAWSEmrClusterConfig,
+				Check:  testAccCheckAWSEmrClusterExists("aws_emr.tf-test-cluster", &jobFlow),
+			},
+		},
+	})
 }
 
 func testAccCheckAWSEmrDestroy(s *terraform.State) error {
-        conn := testAccProvider.Meta().(*AWSClient).emrconn
+	conn := testAccProvider.Meta().(*AWSClient).emrconn
 
-        for _, rs := range s.RootModule().Resources {
-                if rs.Type != "aws_emr" {
-                        continue
-                }
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "aws_emr" {
+			continue
+		}
 
-                params := &emr.DescribeClusterInput{
-                        ClusterId: aws.String(rs.Primary.ID),
-                }
+		params := &emr.DescribeClusterInput{
+			ClusterId: aws.String(rs.Primary.ID),
+		}
 
-                describe, err := conn.DescribeCluster(params)
+		describe, err := conn.DescribeCluster(params)
 
-                if err == nil {
-                        if describe.Cluster != nil &&
-                                *describe.Cluster.Status.State == "WAITING" {
-                                return fmt.Errorf("EMR Cluster still exists")
-                        }
-                }
+		if err == nil {
+			if describe.Cluster != nil &&
+				*describe.Cluster.Status.State == "WAITING" {
+				return fmt.Errorf("EMR Cluster still exists")
+			}
+		}
 
-                providerErr, ok := err.(awserr.Error)
-                if !ok {
-                        return err
-                }
+		providerErr, ok := err.(awserr.Error)
+		if !ok {
+			return err
+		}
 
-                log.Printf("[ERROR] %v", providerErr)
-        }
+		log.Printf("[ERROR] %v", providerErr)
+	}
 
-        return nil
+	return nil
 }
 
 func testAccCheckAWSEmrClusterExists(n string, v *emr.RunJobFlowOutput) resource.TestCheckFunc {
-        return func(s *terraform.State) error {
-                rs, ok := s.RootModule().Resources[n]
-                if !ok {
-                        return fmt.Errorf("Not found: %s", n)
-                }
-                if rs.Primary.ID == "" {
-                        return fmt.Errorf("No cluster id set")
-                }
-                conn := testAccProvider.Meta().(*AWSClient).emrconn
-                describe, err := conn.DescribeCluster(&emr.DescribeClusterInput{
-                        ClusterId: aws.String(rs.Primary.ID),
-                })
-                if err != nil {
-                        return fmt.Errorf("EMR error: %v", err)
-                }
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No cluster id set")
+		}
+		conn := testAccProvider.Meta().(*AWSClient).emrconn
+		describe, err := conn.DescribeCluster(&emr.DescribeClusterInput{
+			ClusterId: aws.String(rs.Primary.ID),
+		})
+		if err != nil {
+			return fmt.Errorf("EMR error: %v", err)
+		}
 
-                if describe.Cluster != nil &&
-                        *describe.Cluster.Id != rs.Primary.ID {
-                        return fmt.Errorf("EMR cluser not found")
-                }
+		if describe.Cluster != nil &&
+			*describe.Cluster.Id != rs.Primary.ID {
+			return fmt.Errorf("EMR cluser not found")
+		}
 
-                if describe.Cluster != nil &&
-                        *describe.Cluster.Status.State != "WAITING" {
-                        return fmt.Errorf("EMR cluser is not up yet")
-                }
+		if describe.Cluster != nil &&
+			*describe.Cluster.Status.State != "WAITING" {
+			return fmt.Errorf("EMR cluser is not up yet")
+		}
 
-                return nil
-        }
+		return nil
+	}
 }
 
 var testAccAWSEmrClusterConfig = fmt.Sprintf(`
